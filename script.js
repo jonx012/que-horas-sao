8a786aceb14283b90895557868f73ba08dbf25e0
João <72712307+jonx012@users.noreply.github.com>
2022-12-27 00:41:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    var msg = document.querySelector("#msg")
+    var img = document.querySelector("#imagem")
+    var data = new Date()
+    var hora = 18
+    var minute = data.getMinutes()
+    
+    msg.innerHTML = `Agora são ${hora}:${minute}`
+    
+    if (hora >= 4 && hora < 12) {
+        // BOM DIA !!!
+        img.src = './img/manha.png'
+        document.body.style.background = "#d4d85b"
+        document.querySelector("#imagem").style.border = "3px solid #d4d85b"
+    } else if (hora < 18) {
+        // BOA TARDE !!!
+        img.src = './img/tarde.png'
+        document.body.style.background = "#e74719"
+        document.querySelector("#imagem").style.border = "3px solid #e74719"
+    } else {
+        // BOA NOITE !!!
+        img.src = './img/noite.png'
+        document.body.style.background = "#082432"
+        document.querySelector("#imagem").style.border = "3px solid #082432"
+    }
+}
